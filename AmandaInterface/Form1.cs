f64024147cf1f679b97a46ab4c8b6968de797f6a
Remco <remcodebruin@gmx.net>
2013-04-23 22:33:42 +0200

    Opnieuw gestart

---
+++ AmandaInterface/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AmandaInterface
+{
+    public partial class Form1 : Form
+    {
+        // ExecuteCommand() staat in amcon.c!
+
+       [DllImport("AmandaCore.dll")]
+        public static extern IntPtr ExecuteCommand(char[] input);
+        [DllImport("AmandaCore.dll", EntryPoint="Load", CallingConvention = CallingConvention.Cdecl)]
+        public static extern bool Loada(char[] file);
+       [DllImport("AmandaCore.dll")]
+        public static extern void CheckIO();
+       [DllImport("AmandaCore.dll")]
+        public static extern void CreateInterpreter();
+       [DllImport("AmandaCore.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Interpret(char[] expr);
+ 
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            String amandaInput = "testfunctie a b = a + b";
+            CreateInterpreter();
+            Interpret("[1]".ToCharArray());
+           IntPtr ptr = ExecuteCommand(amandaInput.ToCharArray());
+           CheckIO();
+            Loada(amandaInput.ToCharArray());
+            
+           string testAnsi = Marshal.PtrToStringAnsi(ptr);
+       
+            showTextBox.Text = testAnsi;
+        }
+    }
+}
--- src/core/bool.h
+++
@@ -1,9 +0,0 @@
-#ifndef BOOL_H
-#define BOOL_H
-
-typedef int bool;
-
-#define True  1
-#define False 0
-
-#endif
