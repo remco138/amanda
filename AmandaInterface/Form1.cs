68c85f03c2ca61ac232408210b25cd954cb1d57a
Remco <remcodebruin@gmx.net>
2013-05-13 22:03:24 +0200

    "" and '' will be highlighted, fixed indenting, version update for submodule FastColoredTextBox

--- AmandaInterface/Form1.cs
+++ AmandaInterface/Form1.cs
@@ -102,54 +102,41 @@ namespace AmandaInterface
         }
 
 
-        private Stack<int> WhereIndents = new Stack<int>();
-        private Stack<int> ConditionIndents = new Stack<int>();
-
-        private int indentSize = 0;
-
-        static bool isInCondition = false;
         private void LoadTextbox_AutoIndentNeeded(object sender, AutoIndentEventArgs e)
         {
             Match isIfRegex = Regex.Match(e.LineText.Trim(), ",* if");
             Match isOtherwiseRegex = Regex.Match(e.LineText.Trim(), ",* otherwise");
             Match isWhereRegex = Regex.Match(e.LineText, "where");
+            int currentIndent = e.LineText.TakeWhile(q => q == ' ').Count();    //shouldn't be too inefficient
 
+            /*
+             *              All these todo's might not be neccessary, it's pretty good right now
+             */
             if (isWhereRegex.Success)
             {
                 e.ShiftNextLines = e.TabLength;
-                indentSize += e.TabLength;
             }
-            if (isIfRegex.Success == true)
+
+             if (isIfRegex.Success == true)
              {
-                try
-                {
-                    if(e.LineText.Trim()[0] != '=')
-                    {
-                        int at = e.LineText.IndexOf('=');
-                        //e.ShiftNextLines = at - ((e.iLine != 0) ? e.iLine : 0);
-                        e.ShiftNextLines = e.TabLength; //FAIL
-                        //LoadTextbox.DoAutoIndent(at);
-                        isInCondition = true;
-                        //else e.ShiftNextLines -= e.TabLength;
-                    }
-                }
-                catch(Exception wat)
-                {
-                    //fucking exceptions
-                }
+                 int at = e.LineText.IndexOf('=');
+                 e.ShiftNextLines = at - currentIndent;
             }
+             if (isOtherwiseRegex.Success)
+                 //(CurrentFunctionContainsPatternMatching() && !previousLineIsCondition)
+             {
+                 //go back to the function's original indent-level, instead of matching the '='
+             }
 
-
-           // if (e.LineText.Trim().Length > 0 && )
-            //{
-                //int at = e.LineText.IndexOf('=');
-                //e.ShiftNextLines = e.Shift = at;
-            //}
             if (e.LineText.Trim() == "" || isOtherwiseRegex.Success == true)
             {
+                if (false)
+                    //|| PreviousLineIsCondition
+                {
+                 //go back to the function's original indent-level, instead of matching the '='
+                }
+
                 e.ShiftNextLines = -e.TabLength;
-                indentSize -= e.TabLength;
-                isInCondition = false;
                 return;
             }
         }
@@ -165,10 +152,46 @@ namespace AmandaInterface
             e.ChangedRange.ClearStyle(KeywordStyle, CommentStyle, ConstantStyle);
 
             e.ChangedRange.SetStyle(KeywordStyle,  @"\b(where|if|else|True|False|otherwise)\b");
-            e.ChangedRange.SetStyle(CommentStyle,  @"\|\|.*");
-            e.ChangedRange.SetStyle(ConstantStyle, @"-?[0-9]+\b");
+            e.ChangedRange.SetStyle(CommentStyle,  @"\|\|.*");                          //comments ||...
+            e.ChangedRange.SetStyle(ConstantStyle, @"(\B-)?[0-9]+\b");                  //numbers 123, -123, to be removed?
+            e.ChangedRange.SetStyle(ConstantStyle, @"""[^""\\]*(?:\\.[^""\\]*)*""?");   //string "", source: stackoverflow
+            e.ChangedRange.SetStyle(ConstantStyle, @"'[^'\\]*(?:\\.[^'\\]*)*'?");       //char ''
         }
 
 
     }
+
+
+    //Will do simple parsing of amanda code, fetches stuff about functions and other tags
+    //will also be of use for smart indenting and make life easier coding (basic)error detection
+    class AmandaTagParser
+    {
+        public List<AmandaTag> AmandaTags;
+
+        public AmandaTagParser()
+        {
+            AmandaTags = new List<AmandaTag>();
+        }
+
+        //generates the tags (function names, variables, etc)
+        public void parse(string code) { }
+
+        //Loads a database file which contains summaries for (hardcoded) amanda functions
+        public void LoadDB(string file) { }
+
+    }
+
+    class AmandaTag
+    {
+        //TagType tagType; //function, weird OOP structure (::=) and possibly more?
+
+        string Name;            //function name, like "increase"
+        List<string> Arguments; //Arguments like: x y (z:xs)
+        string Definition;      //code after the '='
+        string Summary;         //Description of the function, this should be fetched from some DB file (minardus?)
+        Point BeginLocation;    //as in line (x, y) 
+        Point EndLocation;
+ 
+    }
+
 }
