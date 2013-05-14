76650a95bb18befb06f0ae090dbb2873b7e8196b
Callan <Callan.kandasamy@gmail.com>
2013-05-14 11:08:29 +0200

    InterFace Design

--- AmandaInterface/Form1.Designer.cs
+++ AmandaInterface/Form1.Designer.cs
@@ -29,42 +29,35 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.RunTextbox = new System.Windows.Forms.TextBox();
-            this.RunButton = new System.Windows.Forms.Button();
-            this.LoadButton = new System.Windows.Forms.Button();
             this.LoadTextbox = new FastColoredTextBoxNS.FastColoredTextBox();
             this.OutputTextbox = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eDITToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pROJECTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.runButton = new System.Windows.Forms.ToolStripButton();
+            this.loadButton = new System.Windows.Forms.ToolStripButton();
             ((System.ComponentModel.ISupportInitialize)(this.LoadTextbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.OutputTextbox)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // RunTextbox
             // 
-            this.RunTextbox.Location = new System.Drawing.Point(39, 32);
+            this.RunTextbox.Location = new System.Drawing.Point(0, 52);
             this.RunTextbox.Name = "RunTextbox";
-            this.RunTextbox.Size = new System.Drawing.Size(400, 20);
+            this.RunTextbox.Size = new System.Drawing.Size(583, 20);
             this.RunTextbox.TabIndex = 0;
             this.RunTextbox.Text = "getEven";
-            // 
-            // RunButton
-            // 
-            this.RunButton.Location = new System.Drawing.Point(445, 26);
-            this.RunButton.Name = "RunButton";
-            this.RunButton.Size = new System.Drawing.Size(40, 23);
-            this.RunButton.TabIndex = 3;
-            this.RunButton.Text = "Run";
-            this.RunButton.UseVisualStyleBackColor = true;
-            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
-            // 
-            // LoadButton
-            // 
-            this.LoadButton.Location = new System.Drawing.Point(419, 238);
-            this.LoadButton.Name = "LoadButton";
-            this.LoadButton.Size = new System.Drawing.Size(66, 54);
-            this.LoadButton.TabIndex = 4;
-            this.LoadButton.Text = "Load";
-            this.LoadButton.UseVisualStyleBackColor = true;
-            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
             // 
             // LoadTextbox
             // 
@@ -75,11 +68,11 @@
             this.LoadTextbox.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.LoadTextbox.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
             this.LoadTextbox.IsReplaceMode = false;
-            this.LoadTextbox.Location = new System.Drawing.Point(39, 58);
+            this.LoadTextbox.Location = new System.Drawing.Point(0, 78);
             this.LoadTextbox.Name = "LoadTextbox";
             this.LoadTextbox.Paddings = new System.Windows.Forms.Padding(0);
             this.LoadTextbox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.LoadTextbox.Size = new System.Drawing.Size(374, 374);
+            this.LoadTextbox.Size = new System.Drawing.Size(583, 659);
             this.LoadTextbox.TabIndex = 5;
             this.LoadTextbox.Text = "getEven = [x | x <- [0..100]; x % 2 = 0]";
             this.LoadTextbox.Zoom = 100;
@@ -97,27 +90,125 @@
             this.OutputTextbox.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.OutputTextbox.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
             this.OutputTextbox.IsReplaceMode = false;
-            this.OutputTextbox.Location = new System.Drawing.Point(489, 58);
+            this.OutputTextbox.Location = new System.Drawing.Point(589, 52);
             this.OutputTextbox.Name = "OutputTextbox";
             this.OutputTextbox.Paddings = new System.Windows.Forms.Padding(0);
             this.OutputTextbox.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.OutputTextbox.Size = new System.Drawing.Size(312, 374);
+            this.OutputTextbox.Size = new System.Drawing.Size(595, 685);
             this.OutputTextbox.TabIndex = 6;
             this.OutputTextbox.Text = "Yo!";
             this.OutputTextbox.Zoom = 100;
             this.OutputTextbox.Load += new System.EventHandler(this.OutputTextbox_Load);
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem,
+            this.eDITToolStripMenuItem,
+            this.pROJECTToolStripMenuItem,
+            this.hELPToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1184, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.fILEToolStripMenuItem.Text = "FILE";
+            // 
+            // eDITToolStripMenuItem
+            // 
+            this.eDITToolStripMenuItem.Name = "eDITToolStripMenuItem";
+            this.eDITToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.eDITToolStripMenuItem.Text = "EDIT";
+            // 
+            // pROJECTToolStripMenuItem
+            // 
+            this.pROJECTToolStripMenuItem.Name = "pROJECTToolStripMenuItem";
+            this.pROJECTToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.pROJECTToolStripMenuItem.Text = "PROJECT";
+            // 
+            // hELPToolStripMenuItem
+            // 
+            this.hELPToolStripMenuItem.Name = "hELPToolStripMenuItem";
+            this.hELPToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.hELPToolStripMenuItem.Text = "HELP";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 740);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1184, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runButton,
+            this.loadButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1184, 25);
+            this.toolStrip1.TabIndex = 9;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // runButton
+            // 
+            this.runButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.runButton.Image = ((System.Drawing.Image)(resources.GetObject("runButton.Image")));
+            this.runButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.runButton.Name = "runButton";
+            this.runButton.Size = new System.Drawing.Size(23, 22);
+            this.runButton.Text = "runtoolStripButton1";
+            this.runButton.Click += new System.EventHandler(this.runButton_Click_1);
+            // 
+            // loadButton
+            // 
+            this.loadButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.loadButton.Image = ((System.Drawing.Image)(resources.GetObject("loadButton.Image")));
+            this.loadButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(23, 22);
+            this.loadButton.Text = "Load";
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click_1);
+            // 
             // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(813, 444);
+            this.ClientSize = new System.Drawing.Size(1184, 762);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.OutputTextbox);
             this.Controls.Add(this.LoadTextbox);
-            this.Controls.Add(this.LoadButton);
-            this.Controls.Add(this.RunButton);
             this.Controls.Add(this.RunTextbox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.LoadTextbox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.OutputTextbox)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -126,10 +217,19 @@
         #endregion
 
         public System.Windows.Forms.TextBox RunTextbox;
-        public System.Windows.Forms.Button RunButton;
-        public System.Windows.Forms.Button LoadButton;
         public FastColoredTextBoxNS.FastColoredTextBox LoadTextbox;
         public FastColoredTextBoxNS.FastColoredTextBox OutputTextbox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eDITToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pROJECTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hELPToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton runButton;
+        private System.Windows.Forms.ToolStripButton loadButton;
 
     }
 }
