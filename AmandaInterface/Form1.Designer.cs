87d0979900089b0399840ed9a4a2e368d4ce8a23
Rutger Schoorstra <schoorstra_rutger@hotmail.com>
2013-06-04 14:04:23 +0200

    deleted the this.deleteToolStripMenuItem.ShortcutKeys line

--- AmandaInterface/Form1.Designer.cs
+++ AmandaInterface/Form1.Designer.cs
@@ -239,7 +239,6 @@
             // deleteToolStripMenuItem
             // 
             this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            this.deleteToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.Delete;
             this.deleteToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
             this.deleteToolStripMenuItem.Text = "Delete";
             this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
